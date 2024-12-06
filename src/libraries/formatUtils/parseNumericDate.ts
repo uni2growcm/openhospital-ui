fcfe85d4b68c8a454f45185aa5e9264ed20b68cf
SteveGT96 <steve.tsala@intesys.it>
2024-12-06 16:25:42 +0100

    chore: Update parseNumericDate function to use moment

--- src/libraries/formatUtils/parseNumericDate.ts
+++ src/libraries/formatUtils/parseNumericDate.ts
@@ -1,12 +1,17 @@
+import moment from "moment";
+
 /**
  * Parse a date from 'YYYYMMDD' format
  * @param value - The value to parse
  * @returns the parsed date or null if the format wasn't correct
  */
 export function parseNumericDate(value: string) {
-  if (!/^(\d){8}$/.test(value)) return null;
-  const year = parseInt(value.slice(0, 4)),
-    month = parseInt(value.slice(4, 6)),
-    day = parseInt(value.slice(6, 8));
-  return new Date(year, month, day);
+  if (!/^(\d){8}$/.test(value)) {
+    try {
+      return moment(value).toDate();
+    } catch {
+      return null;
+    }
+  }
+  return moment(value, "YYYYMMDD").toDate();
 }
