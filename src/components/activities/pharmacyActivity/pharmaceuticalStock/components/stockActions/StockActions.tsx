fb4b37b89a95cf926505d4a542e213270d9838f9
Tatemsa <tatemsabill@gmail.com>
2025-11-17 15:34:19 +0100

    Add e2e tests for movement discharge

--- src/components/activities/pharmacyActivity/pharmaceuticalStock/components/stockActions/StockActions.tsx
+++ src/components/activities/pharmacyActivity/pharmaceuticalStock/components/stockActions/StockActions.tsx
@@ -29,14 +29,15 @@ export function StockActions() {
       </Button>
       <div className="separator"></div>
       <Link to={"./discharge-movement"}>
-      <Button
-        type="button"
-        variant="contained"
-        className="discharge_button"
-        startIcon={<LogoutIcon sx={{ transform: "rotate(90deg)" }} />}
-      >
-        {t("pharmacy.stock.discharge")}
-      </Button>
+        <Button
+          type="button"
+          variant="contained"
+          dataCy="discharge-button"
+          className="discharge_button"
+          startIcon={<LogoutIcon sx={{ transform: "rotate(90deg)" }} />}
+        >
+          {t("pharmacy.stock.discharge")}
+        </Button>
       </Link>
       <Link to={"./charge-movement"}>
         <Button
