0bd8920890596aa0374d23639ade72a966fa7b5f
serene21 <hemaserenankemenyi@gmail.com>
2025-11-27 14:31:36 +0100

    revert changes on StockActions

--- src/components/activities/pharmacyActivity/pharmaceuticalStock/components/stockActions/StockActions.tsx
+++ src/components/activities/pharmacyActivity/pharmaceuticalStock/components/stockActions/StockActions.tsx
@@ -1,61 +1,54 @@
 import ExitToAppIcon from "@mui/icons-material/ExitToApp";
 import LogoutIcon from "@mui/icons-material/Logout";
 import Button from "components/accessories/button/Button";
-import { MovementDTO } from "generated";
-import React, { useState } from "react";
+import React from "react";
 import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
 import "./styles.scss";
 
-export interface StockActionsHandle {
-  handleAdjustClick: (movement: MovementDTO) => void;
+export function StockActions() {
+  const { t } = useTranslation();
+
+  return (
+    <div className="pharmaceuticalStock__actions">
+      <Button
+        className="export_button"
+        type="button"
+        variant="outlined"
+        color="inherit"
+      >
+        {t("pharmacy.stock.exportList")}
+      </Button>
+      <Button
+        className="report_button"
+        type="button"
+        variant="outlined"
+        color="inherit"
+      >
+        {t("pharmacy.stock.stockReport")}
+      </Button>
+      <div className="separator"></div>
+      <Link to={"./discharge-movement"}>
+        <Button
+          type="button"
+          variant="contained"
+          className="discharge_button"
+          startIcon={<LogoutIcon sx={{ transform: "rotate(90deg)" }} />}
+        >
+          {t("pharmacy.stock.discharge")}
+        </Button>
+      </Link>
+      <Link to={"./charge-movement"}>
+        <Button
+          className="charge_button"
+          dataCy="charge-button"
+          type="button"
+          variant="contained"
+          startIcon={<ExitToAppIcon sx={{ transform: "rotate(-90deg)" }} />}
+        >
+          {t("pharmacy.stock.charge")}
+        </Button>
+      </Link>
+    </div>
+  );
 }
-
-export const StockActions = React.forwardRef<StockActionsHandle, {}>(
-  (_, ref) => {
-    const { t } = useTranslation();
-
-    return (
-      <div className="pharmaceuticalStock__actions">
-        <Button
-          className="export_button"
-          type="button"
-          variant="outlined"
-          color="inherit"
-        >
-          {t("pharmacy.stock.exportList")}
-        </Button>
-        <Button
-          className="report_button"
-          type="button"
-          variant="outlined"
-          color="inherit"
-        >
-          {t("pharmacy.stock.stockReport")}
-        </Button>
-        <div className="separator"></div>
-        <Link to={"./discharge-movement"}>
-          <Button
-            type="button"
-            variant="contained"
-            className="discharge_button"
-            startIcon={<LogoutIcon sx={{ transform: "rotate(90deg)" }} />}
-          >
-            {t("pharmacy.stock.discharge")}
-          </Button>
-        </Link>
-        <Link to={"./charge-movement"}>
-          <Button
-            className="charge_button"
-            dataCy="charge-button"
-            type="button"
-            variant="contained"
-            startIcon={<ExitToAppIcon sx={{ transform: "rotate(-90deg)" }} />}
-          >
-            {t("pharmacy.stock.charge")}
-          </Button>
-        </Link>
-      </div>
-    );
-  }
-);
