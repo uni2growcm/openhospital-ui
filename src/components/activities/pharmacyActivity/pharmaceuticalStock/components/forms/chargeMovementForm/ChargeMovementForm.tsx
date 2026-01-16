3b900edbacc87c01501be879765ac63c7d1f02b8
Aurorj <aurellejoellen@gmail.com>
2026-01-16 14:03:42 +0100

    update/OH2-228 add quantity in lotformfield component

--- src/components/activities/pharmacyActivity/pharmaceuticalStock/components/forms/chargeMovementForm/ChargeMovementForm.tsx
+++ src/components/activities/pharmacyActivity/pharmaceuticalStock/components/forms/chargeMovementForm/ChargeMovementForm.tsx
@@ -112,7 +112,7 @@ export function ChargeMovementForm({
             control={control}
             medical={formatedValues.medical}
             name="lot"
-            showNewLotOption
+            showNewLotOption={true}
           />
         )}
         <div className="col-start-1 col-span-full"></div>
