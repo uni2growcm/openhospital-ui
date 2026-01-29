ea983b3d1dbb1d34a5884513106851872b52ce5b
duval2dev <duval_donfack@uni2growcameroun.com>
2026-01-29 16:30:51 +0100

    Modify search patient function to fix e2e test

--- src/state/patients/slice.ts
+++ src/state/patients/slice.ts
@@ -31,7 +31,9 @@ export const patientSlice = createSlice({
         state.searchResults = ApiResponse.loading();
       })
       .addCase(thunks.searchPatient.fulfilled, (state, action) => {
-        state.searchResults = ApiResponse.value(action.payload);
+        state.searchResults = isEmpty(action.payload)
+          ? ApiResponse.empty()
+          : ApiResponse.value(action.payload);
       })
       .addCase(thunks.searchPatient.rejected, (state, action) => {
         state.searchResults = ApiResponse.error(action.payload);
