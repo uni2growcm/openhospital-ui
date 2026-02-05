adfa679a6db6c9de10edc69398ba50fa06c3dd3f
duval2dev <duval_donfack@uni2growcameroun.com>
2026-02-05 09:07:30 +0100

    (OH2-276) Fix export list button in Parmaceutical stock

--- src/components/activities/pharmacyActivity/pharmaceuticalStock/components/stockActions/StockActions.tsx
+++ src/components/activities/pharmacyActivity/pharmaceuticalStock/components/stockActions/StockActions.tsx
@@ -10,8 +10,11 @@ import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
 import { printPharmaceuticalStockPdf } from "state/pharmacy";
 import "./styles.scss";
+import ConfirmationDialog from "components/accessories/confirmationDialog/ConfirmationDialog";
+import warningIcon from "../../../../../../assets/warning-icon.png";
+import * as XLSX from "xlsx";
 
-export function StockActions() {
+export function StockActions({ dataToExport }: { dataToExport: any[] }) {
   const { t } = useTranslation();
   const dispatch = useAppDispatch();
 
@@ -35,6 +38,36 @@ export function StockActions() {
     setIsPrint(false);
   };
 
+  const [noPageToExport, setNoPageToExport] = useState<boolean>(false);
+  const handlePrintExportListExcel = () => {
+    if (!dataToExport) return;
+    if (dataToExport.length === 0) {
+      setNoPageToExport(true);
+      return;
+    }
+
+    const excelRows = dataToExport.map((item) => ({
+      [t("pharmacy.stock.refNo")]: item.refNo, 
+      [t("pharmacy.stock.type")]: item.type, 
+      [t("pharmacy.stock.origin")]: item.origin || "", 
+      [t("pharmacy.stock.quantity")]: item.quantity, 
+      [t("pharmacy.stock.medical")]: item.medical, 
+      [t("pharmacy.stock.medicalType")]: item.medicalType || "", 
+      [t("pharmacy.stock.cost")]: item.cost || "",
+      [t("pharmacy.stock.total")]: item.quantity * (item.cost || 0), 
+      [t("pharmacy.stock.lot")]: item.lot || "", 
+      [t("pharmacy.stock.prepDate")]: item.prepDate || "", 
+      [t("pharmacy.stock.expDate")]: item.expDate || "", 
+    }));
+    const worksheet = XLSX.utils.json_to_sheet(excelRows);
+    const workbook = XLSX.utils.book_new();
+    
+    const date = new Date().getTime();
+    const dateStr = new Date().toISOString().split("T")[0];
+    XLSX.utils.book_append_sheet(workbook, worksheet, `Stock-${dateStr}`);
+    XLSX.writeFile(workbook, `pharmaceutical-stock-export-${dateStr}-${date}.xlsx`);
+  };
+
   return (
     <div className="pharmaceuticalStock__actions">
       <Button
@@ -42,6 +75,7 @@ export function StockActions() {
         type="button"
         variant="outlined"
         color="inherit"
+        onClick={handlePrintExportListExcel}
       >
         {t("pharmacy.stock.exportList")}
       </Button>
@@ -85,6 +119,15 @@ export function StockActions() {
         secondaryButtonLabel={t("common.cancel")}
         handlePrimaryButtonClick={handleGetReport}
         handleSecondaryButtonClick={() => setIsPrint(false)}
+      />
+      <ConfirmationDialog
+        isOpen={noPageToExport}
+        title={t("pharmacy.stock.noPageToExport.title")}
+        info={t("pharmacy.stock.noPageToExport.description")}
+        icon={warningIcon}
+        primaryButtonLabel={t("common.ok")}
+        handlePrimaryButtonClick={() => setNoPageToExport(false)}
+        handleSecondaryButtonClick={() => {}}
       />
     </div>
   );
