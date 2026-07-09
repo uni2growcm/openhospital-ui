8baef53cf3951ded25a20ce962126cb9492769ef
Tatemsa <tatemsabill@gmail.com>
2026-07-09 19:00:31 +0200

    Refactoring

--- src/mockServer/fixtures/diseasesDTO.js
+++ src/mockServer/fixtures/diseasesDTO.js
@@ -43,4 +43,48 @@ export const diseasesDTO = [
     ipdInInclude: true,
     ipdOutInclude: false,
   },
+  {
+    code: 30,
+    description: "Malaria",
+    diseaseType: {
+      code: "IN",
+      description: "1.INFECTIONS AND PARASITIC DISEASES",
+    },
+    opdInclude: true,
+    ipdInInclude: true,
+    ipdOutInclude: true,
+  },
+  {
+    code: 31,
+    description: "Pneumonia",
+    diseaseType: {
+      code: "RE",
+      description: "2.RESPIRATORY DISEASES",
+    },
+    opdInclude: true,
+    ipdInInclude: false,
+    ipdOutInclude: true,
+  },
+  {
+    code: 32,
+    description: "Anemia",
+    diseaseType: {
+      code: "BL",
+      description: "5.BLOOD DISEASES",
+    },
+    opdInclude: false,
+    ipdInInclude: false,
+    ipdOutInclude: false,
+  },
+  {
+    code: 33,
+    description: "Diarrhea",
+    diseaseType: {
+      code: "DI",
+      description: "6.DIGESTIVE DISEASES",
+    },
+    opdInclude: true,
+    ipdInInclude: true,
+    ipdOutInclude: false,
+  },
 ];
