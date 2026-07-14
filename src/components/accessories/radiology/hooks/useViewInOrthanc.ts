a2e09b68563e05a1e8866d953e6b2e38c1bd65d8
duval2dev <duval_donfack@uni2growcameroun.com>
2026-07-14 19:45:10 +0100

    Fix orthanc config

--- src/components/accessories/radiology/hooks/useViewInOrthanc.ts
+++ src/components/accessories/radiology/hooks/useViewInOrthanc.ts
@@ -5,7 +5,7 @@ export const useViewInOrthanc = (level: 'study' | 'series' | 'instance') => {
 	 * Todo: Use value provided by the backend
 	 */
 	const orthancExplorerUrl =
-		process.env.REACT_APP_ORTHANC_EXPLORER_URL ||
+		import.meta.env.VITE_APP_ORTHANC_EXPLORER_URL ||
 		'https://orthanc.uni2growcameroun.com/app/explorer.html';
 	const handleViewInOrthanc = useCallback(
 		(row: any) => () => {
